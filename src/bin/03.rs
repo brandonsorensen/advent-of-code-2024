ef0ff0c4881a2c690d00c8b7b30f7fc2268c853e
Brandon Sorensen <brandon.sorensen@stepstone.com>
2024-12-03 12:39:36 +0100

    use tuples

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -5,29 +5,47 @@ use {
   std::sync::OnceLock,
 };
 
+/* PART 1 */
+
 pub fn part_one(input: &str) -> Option<u32> {
   let output = input
     .lines()
     .flat_map(|line| {
       mul_regex().captures_iter(line).map(|cap| {
         let (_, [lhs, rhs]) = cap.extract();
-        Operation {
-          lhs: lhs.parse::<u32>().expect("couldn't parse input"),
-          rhs: rhs.parse::<u32>().expect("couldn't parse input"),
-        }
+        (
+          lhs.parse::<u32>().expect("couldn't parse input"),
+          rhs.parse::<u32>().expect("couldn't parse input"),
+        )
       })
     })
-    .map(|op| op.lhs * op.rhs)
+    .map(|(lhs, rhs)| lhs * rhs)
     .sum();
   Some(output)
 }
 
+fn mul_regex() -> &'static Regex {
+  static MUL_REGEX: OnceLock<Regex> = OnceLock::new();
+  MUL_REGEX.get_or_init(|| Regex::new(r"mul\((\d+),(\d+)\)").unwrap())
+}
+
+/* PART 2 */
+
 pub fn part_two(input: &str) -> Option<u32> {
-  let output = input
+  Some(part_two_no_opt(input))
+}
+
+pub fn part_two_no_opt(input: &str) -> u32 {
+  input
     .lines()
     .flat_map(|line| mul_regex_ext().captures_iter(line))
     .fold(State::default(), |state, cap| {
+      // 3 = do, 4 = n't, 5 = lhs, 6 = rhs
       match (cap.get(3), cap.get(4), cap.get(5), cap.get(6)) {
+        (None, None, Some(lhs), Some(rhs)) if state.enabled => State {
+          sum: state.sum + match_to_int(lhs) * match_to_int(rhs),
+          enabled: state.enabled,
+        },
         (Some(_do), None, None, None) => State {
           sum: state.sum,
           enabled: true,
@@ -36,16 +54,11 @@ pub fn part_two(input: &str) -> Option<u32> {
           sum: state.sum,
           enabled: false,
         },
-        (None, None, Some(lhs), Some(rhs)) if state.enabled => State {
-          sum: state.sum + match_to_int(lhs) * match_to_int(rhs),
-          enabled: state.enabled,
-        },
         (None, None, Some(_lhs), Some(_rhs)) => state,
         _ => unreachable!(),
       }
     })
-    .sum;
-  Some(output)
+    .sum
 }
 
 fn match_to_int(match_: Match<'_>) -> u32 {
@@ -70,20 +83,9 @@ impl Default for State {
   }
 }
 
-#[derive(Debug)]
-struct Operation {
-  lhs: u32,
-  rhs: u32,
-}
-
-fn mul_regex() -> &'static Regex {
-  static MUL_REGEX: OnceLock<Regex> = OnceLock::new();
-  MUL_REGEX.get_or_init(|| Regex::new(r"mul\((\d+),(\d+)\)").unwrap())
-}
-
 fn mul_regex_ext() -> &'static Regex {
   static MUL_REGEX_EXT: OnceLock<Regex> = OnceLock::new();
-  MUL_REGEX_EXT.get_or_init(|| Regex::new(r"(((do(n't)?)[^mul]*)|mul\((\d+),(\d+)\))").unwrap())
+  MUL_REGEX_EXT.get_or_init(|| Regex::new(r"(((do(n't)?))|mul\((\d+),(\d+)\))").unwrap())
 }
 
 #[cfg(test)]
