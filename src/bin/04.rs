ce9d9298216e2343cd1950804c80c67bef1b5cd3
Brandon Sorensen <brandon.sorensen@stepstone.com>
2024-12-04 09:37:20 +0100

    basic sketch and add ndarray

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,42 @@
 advent_of_code::solution!(4);
 
+const LINE_LENGTH: usize = 140;
+const TARGET_WORD: &str = "XMAS";
+
 pub fn part_one(input: &str) -> Option<u32> {
-  None
+  Some(part_one_no_opt(input))
+}
+
+fn part_one_no_opt(input: &str) -> u32 {
+  let array = initialize_array(input);
+  let (n_rows, n_cols) = array.dim();
+  itertools::iproduct!(0..n_rows, 0..n_cols)
+    .map(|(row, column)| {
+      let target_char = array.get((row, column)).expect("index out of bounds");
+      match target_char {
+        'X' => {
+          todo!()
+        }
+        'S' => {
+          todo!()
+        }
+        _ => 0,
+      }
+    })
+    .sum()
+}
+
+fn initialize_array(input: &str) -> ndarray::Array2<char> {
+  let mut n_lines = 0;
+  let char_stream = input
+    .lines()
+    .flat_map(|line| {
+      n_lines += 1;
+      line.chars()
+    })
+    .collect::<Vec<_>>();
+  ndarray::Array2::from_shape_vec((n_lines, LINE_LENGTH), char_stream)
+    .expect("couldn't initialize array")
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
